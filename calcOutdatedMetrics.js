c1ce8ad98e185c6b5bde564f278d70805ad72a86
Don Leistman <donleistman@gmail.com>
2022-05-03 18:52:27 -0400

    Parse package.json to get number of packages

--- calcOutdatedMetrics.js
+++ calcOutdatedMetrics.js
@@ -1,69 +1,78 @@
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-let data = '';
+const { exec } = require("child_process");
 
-process.stdin.on('data', function (chunk) {
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+let data = "";
+
+process.stdin.on("data", function (chunk) {
   data += chunk;
 });
 
-process.stdin.on('end', function () {
-  const jsonLines = data.split('\n');
-  const lineWithDependencyTable = jsonLines.find(json =>
-    json.includes('{"type":"table"'),
-  );
+process.stdin.on("end", function () {
+  exec(
+    "cat package.json | jq '.dependencies, .devDependencies | keys | length' | paste -s -d+ - | bc",
+    (err, stdout) => {
+      const jsonLines = data.split("\n");
+      const lineWithDependencyTable = jsonLines.find((json) =>
+        json.includes('{"type":"table"')
+      );
 
-  if (!lineWithDependencyTable) {
-    console.log('data:', data);
-    throw new Error(
-      `Did not receive output from 'yarn outdated'. data: ${data}`,
-    );
-  }
-  const parsedData = JSON.parse(lineWithDependencyTable);
-  const packageList = parsedData.data.body;
-
-  const numberOfPackages = packageList?.length;
-
-  const upToDate = [];
-  const behindPatch = [];
-  const behindMinor = [];
-  const behindMajor = [];
-
-  packageList?.forEach(package => {
-    const [name, currentVersion, wanted, latestVersion] = package;
-    const [currentMajor, currentMinor, currentPatch] = currentVersion.split('.');
-    const [latestMajor, latestMinor, latestPatch] = latestVersion.split('.');
-
-    if (currentMajor >= latestMajor) {
-      if (currentMinor >= latestMinor) {
-        if (currentPatch >= latestPatch) {
-          upToDate.push(package);
-        } else {
-          behindPatch.push(package);
-        }
-      } else {
-        behindMinor.push(package);
+      if (!lineWithDependencyTable) {
+        console.log("data:", data);
+        throw new Error(
+          `Did not receive output from 'yarn outdated'. data: ${data}`
+        );
       }
-    } else {
-      behindMajor.push(package);
+      const parsedData = JSON.parse(lineWithDependencyTable);
+      const packageList = parsedData.data.body;
+
+      const numberOfPackages = parseInt(stdout);
+
+      const behindPatch = [];
+      const behindMinor = [];
+      const behindMajor = [];
+
+      packageList?.forEach((package) => {
+        const [name, currentVersion, wanted, latestVersion] = package;
+        const [currentMajor, currentMinor, currentPatch] =
+          currentVersion.split(".");
+        const [latestMajor, latestMinor, latestPatch] =
+          latestVersion.split(".");
+
+        if (currentMajor >= latestMajor) {
+          if (currentMinor >= latestMinor) {
+            if (currentPatch >= latestPatch) {
+              throw new Error("package is not outdated");
+            } else {
+              behindPatch.push(package);
+            }
+          } else {
+            behindMinor.push(package);
+          }
+        } else {
+          behindMajor.push(package);
+        }
+      });
+
+      const numBehindPatch = behindPatch.length;
+      const numBehindMinor = behindMinor.length;
+      const numBehindMajor = behindMajor.length;
+      const numUpToDate =
+        numberOfPackages - numBehindMajor - numBehindMinor - numBehindPatch;
+
+      const percentUpToDate = (numUpToDate / numberOfPackages).toFixed(2);
+      const percentBehindPatch = (numBehindPatch / numberOfPackages).toFixed(2);
+      const percentBehindMinor = (numBehindMinor / numberOfPackages).toFixed(2);
+      const percentBehindMajor = (numBehindMajor / numberOfPackages).toFixed(2);
+
+      const results = {
+        numberOfPackages,
+        percentUpToDate,
+        percentBehindPatch,
+        percentBehindMinor,
+        percentBehindMajor,
+      };
+      process.stdout.write(JSON.stringify(results, null, 4));
     }
-  });
-
-  const numUpToDate = upToDate.length;
-  const numBehindPatch = behindPatch.length;
-  const numBehindMinor = behindMinor.length;
-  const numBehindMajor = behindMajor.length;
-
-  const percentUpToDate = (numUpToDate / numberOfPackages).toFixed(2);
-  const percentBehindPatch = (numBehindPatch / numberOfPackages).toFixed(2);
-  const percentBehindMinor = (numBehindMinor / numberOfPackages).toFixed(2);
-  const percentBehindMajor = (numBehindMajor / numberOfPackages).toFixed(2);
-
-  const results = {
-    numberOfPackages,
-    percentUpToDate,
-    percentBehindPatch,
-    percentBehindMinor,
-    percentBehindMajor,
-  };
-  process.stdout.write(JSON.stringify(results, null, 4));
+  );
 });
